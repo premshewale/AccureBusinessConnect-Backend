ba32202e068cda12dda567bb4e0083d372f2c3b2
Prem Shewale <premshewale47@gmail.com>
2025-10-01 10:47:03 +0530

    Project Backend Setup

---
+++ src/main/java/com/accuresoftech/abc/AccureBusinessConnectApplication.java
@@ -0,0 +1,13 @@
+package com.accuresoftech.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccureBusinessConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccureBusinessConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accuresoftech/abc/AccureBusinessConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accuresoftech.abc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccureBusinessConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
